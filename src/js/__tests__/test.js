aa5e203082fed36184a0749da9c7672bdafa680e
Dmitry Gavrilov <gavrilov.dmitry1985@yandex.ru>
2023-01-17 21:55:16 +1000

    initial commit

---
+++ src/js/__tests__/test.js
@@ -0,0 +1,7 @@
+import GameSavingLoader from '../GameSavingLoader';
+
+test('проверка загрузчика loader', async () => {
+  const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+  const recive = await GameSavingLoader.load();
+  expect(recive).toBe(data);
+});
